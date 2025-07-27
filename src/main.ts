49862764cc79e4e03124fe96abcbdce19db51ceb
FRANZ SCHWARTZ GC <gavino_10@hotmail.es>
2025-07-27 16:48:23 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors({
+    origin:'*',
+    methods:'GET,POST,PUT,DELETE',
+    credentials:true
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
